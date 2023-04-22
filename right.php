65e3539085625a3599602caaa63dcf50a7a69861
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:13 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,8 @@
+<?php
+
+$this->add_right("media", "media", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("media", "media", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("media", "media", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("media", "media", "staff", view:true, add:true, edit:true);
+$this->add_right("media", "media", "registered", view:true, add:true);
+$this->add_right("media", "media", "guest", view:true, );
