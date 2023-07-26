fd8df45701538d73fc24758c9ec6b7551864c916
Dedan Irungu <dedanirungu@gmail.com>
2023-07-26 13:59:52 +0300

    adding listtable and formbuilder fuction to all model so that data can be rendered via json

--- Entities/Media.php
+++ Entities/Media.php
@@ -4,6 +4,8 @@ namespace Modules\Media\Entities;
 
 use Illuminate\Database\Schema\Blueprint;
 use Modules\Base\Entities\BaseModel;
+use Modules\Core\Classes\Views\FormBuilder;
+use Modules\Core\Classes\Views\ListTable;
 
 class Media extends BaseModel
 {
@@ -12,6 +14,38 @@ class Media extends BaseModel
     public $migrationDependancy = [];
     protected $table = "media";
 
+    public function listTable()
+    {
+        // listing view fields
+        $fields = new ListTable();
+
+        $fields->name('name')->type('text')->ordering(true);
+
+        return $fields;
+
+    }
+
+    public function formBuilder()
+    {
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('name')->type('text')->group('w-1/2');
+
+        return $fields;
+
+    }
+
+    public function filter()
+    {
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('name')->type('text')->group('w-1/6');
+
+        return $fields;
+
+    }
     /**
      * List of fields for managing postings.
      *
